71a8e163d883764796d35ea5b8e22146118cd6d8
titusmuthomik <titus.muthomi@tala.co>
2023-07-13 11:20:05 +0300

    Run both jobs concurrently

--- src/main/java/com/batch/demo/BatchApplication.java
+++ src/main/java/com/batch/demo/BatchApplication.java
@@ -1,13 +1,31 @@
 package com.batch.demo;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BatchApplication {
 
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BatchApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		JobParameters params = new JobParametersBuilder()
+				.addString("JobId", String.valueOf(System.currentTimeMillis()))
+				.toJobParameters();
+		jobLauncher.run(job, params);
+	}
 }
